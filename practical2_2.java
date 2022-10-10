d5984c7b6ca9f553256850e6ece5c13ed8959727
21CE020 <113235967+21CE020@users.noreply.github.com>
2022-10-10 21:52:40 +0530

    Add files via upload

---
+++ practical2_2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class practical2_2 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream sourceStream = null;
+        FileOutputStream targetStream = null;
+        try {
+            sourceStream = new FileInputStream(
+                    "D:\\STUDY\\JAVA practical\\Practice set 5 - File Handling & Streams\\1.txt");
+            targetStream = new FileOutputStream(
+                    "D:\\STUDY\\JAVA practical\\Practice set 5 - File Handling & Streams\\2.txt");
+            int temp;
+            while ((temp = sourceStream.read()) != -1)
+                targetStream.write((byte) temp);
+        } finally {
+            if (sourceStream != null)
+                sourceStream.close();
+            if (targetStream != null)
+                targetStream.close();
+        }
+    }
+}
---
+++ practical6.java
@@ -0,0 +1,14 @@
+public class practical6 {
+    public static void main(String[] args) {
+        System.out.println("Method 1 :-");
+        int a = 119;
+        Integer i = Integer.valueOf(a);
+        Integer j = a;
+        System.out.println(a + " " + j + " " + i);
+        System.out.println("\nMethod 2 :-");
+        Integer x = Integer.valueOf(120);
+        Integer y = x;
+        Integer z = x;
+        System.out.println(x + " " + y + " " + z);
+    }
+}
